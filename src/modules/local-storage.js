b6f289c14ef43ad9a60ba1643c93fe7db20edc82
Rithreaksa <wonderfulreaksa@gmail.com>
2021-01-01 15:38:06 +0100

    Modifies useState and useEffect hooks

--- src/modules/local-storage.js
+++ src/modules/local-storage.js
@@ -1,11 +1,13 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export const useLocalStorageObjectStates = (key, defaultValue = {}) => {
-  const [value, setValue] = React.useState(
+  const [value, setValue] = useState (
     () => JSON.parse(localStorage.getItem(key)) || defaultValue
   );
-  React.useEffect(() => {
+  useEffect (() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
   return [value, setValue];
 };
+
+export default useLocalStorageObjectStates
